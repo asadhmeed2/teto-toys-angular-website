209f5e81b973d45f4117afc10bf8e83bcf1a2081
asadhmeed2 <asadhmeed2@gmail.com>
2026-06-24 08:00:04 +0300

    replace hardcodded mascot code in app-footer with reused component

--- src/app/shared/components/app-layout/app-footer/app-footer.component.ts
+++ src/app/shared/components/app-layout/app-footer/app-footer.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { AppMascotComponent } from '../../app-mascot';
 
 @Component({
   selector: 'app-footer',
+  imports: [AppMascotComponent],
   templateUrl: './app-footer.component.html',
   styleUrl: './app-footer.component.scss',
 })
 export class AppFooterComponent {}
+
